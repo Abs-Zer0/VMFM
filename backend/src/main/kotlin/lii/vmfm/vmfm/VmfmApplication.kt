4c2450d589a43fc64853e40643fe15938fd93f7d
Illyasviel <lii-2000@yandex.ru>
2021-05-07 14:56:41 +0500

    init app

---
+++ backend/src/main/kotlin/lii/vmfm/vmfm/VmfmApplication.kt
@@ -0,0 +1,11 @@
+package lii.vmfm.vmfm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VmfmApplication
+
+fun main(args: Array<String>) {
+	runApplication<VmfmApplication>(*args)
+}
---
+++ backend/src/test/kotlin/lii/vmfm/vmfm/VmfmApplicationTests.kt
@@ -0,0 +1,13 @@
+package lii.vmfm.vmfm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VmfmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
